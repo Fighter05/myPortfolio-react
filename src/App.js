2ee5f99a2c7a904b81d60a990affe9fd88428aec
fighter05 <sitthipon.sint@gmail.com>
2022-12-26 10:03:18 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My Portfolio is Work in Progress!
+          Work in Progress! 
         </p>
       </header>
     </div>
