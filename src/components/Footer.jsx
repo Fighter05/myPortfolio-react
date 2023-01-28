e071b466ef93ed6b2c1c856191d8da6294a71ae4
fighter05 <sitthipon.sint@gmail.com>
2023-01-28 14:30:29 +0700

    add responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center h-16 bg-[#1B1C1E]'>
-      <p className='text-gray-300 tracking-widest font-bold'>© 2022 ー <span className='text-yellow-300'>SITTHIPON.</span> All rights reserved. Privacy Policy | Terms & Conditions</p>
+    <div className='flex justify-center items-center h-auto bg-[#1B1C1E] py-4'>
+      <p className='transition ease-in-out duration-300 text-gray-300 tracking-widest font-bold text-center'>© 2023 ー <span className='cursor-pointer text-yellow-300 hover:translate-x-0'>SITTHIPON.</span> All rights reserved. Privacy Policy | Terms & Conditions</p>
     </div>
   )
 }
