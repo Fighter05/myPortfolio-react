b1315f4a70a92fe4d35b8daca2f5d3ef59c1180d
fighter05 <sitthipon.sint@gmail.com>
2023-09-29 09:34:55 +0700

    update skill 2023

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,76 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#1B1C1E] flex justify-center items-center p-4'>
-        {/* <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-yellow-300 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - sitthipon.sint@gmail.com</p>
-            </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 transition ease-in-out hover:bg-yellow-600 hover:border-yellow-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
-        </form> */}
-        <div className='pb-8 '>
-          <p className='text-4xl font-bold inline border-b-4 border-yellow-300 text-gray-300'>Contact</p>
-          <p className='text-gray-300 py-4'>// Get in touch with me by the contact below.</p>
-          <p className='text-gray-300 py-4'>Email - <a href = 'mailto: sitthipon.sint@gmail.com' className='underline decoration-solid text-yellow-300'>sitthipon.sint@gmail.com</a></p>
-          <p className='text-gray-300 py-4'>Line - <span className='text-yellow-300'>fight.sint</span></p>
-          <p className='text-gray-300 py-4'>Tel - <span className='text-yellow-300'>(+66) 928070245</span></p>
-          <p className='text-gray-300 py-4'>Location - <span className='text-yellow-300'>Bangkok, Thailand 10230</span></p>
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#1B1C1E] flex justify-center items-center p-4"
+    >
+      <form
+        // method="POST"
+        // action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-yellow-300 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            // Submit the form below or shoot me an email -
+            sitthipon.sint@gmail.com
+          </p>
         </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="text-white border-2 transition ease-in-out hover:bg-yellow-600 hover:border-yellow-600 px-4 py-3 my-8 mx-auto flex items-center">
+          Let's Collaborate
+        </button>
+      </form>
+      {/* <div className="pb-8 ">
+        <p className="text-4xl font-bold inline border-b-4 border-yellow-300 text-gray-300">
+          Contact
+        </p>
+        <p className="text-gray-300 py-4">
+          // Get in touch with me by the contact below.
+        </p>
+        <p className="text-gray-300 py-4">
+          Email -{" "}
+          <a
+            href="mailto: sitthipon.sint@gmail.com"
+            className="underline decoration-solid text-yellow-300"
+          >
+            sitthipon.sint@gmail.com
+          </a>
+        </p>
+        <p className="text-gray-300 py-4">
+          Line - <span className="text-yellow-300">fight.sint</span>
+        </p>
+        <p className="text-gray-300 py-4">
+          Tel - <span className="text-yellow-300">(+66) 928070245</span>
+        </p>
+        <p className="text-gray-300 py-4">
+          Location -{" "}
+          <span className="text-yellow-300">Bangkok, Thailand 10230</span>
+        </p>
+      </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,76 +1,83 @@
-import React from 'react';
+import React from "react";
 
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import FireBase from '../assets/firebase.png';
-import Flutter from '../assets/flutter.png';
-import Dart from '../assets/dart.png';
-import Mysql from '../assets/mysql.png';
-import Java from '../assets/java.png';
-import Python from '../assets/python.png';
-import Tailwind from '../assets/tailwind.png';
-
-
-
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import TypeScript from "../assets/typescript.png";
+import ReactImg from "../assets/react.png";
+import Flutter from "../assets/flutter.png";
+import Dart from "../assets/dart.png";
+import Mysql from "../assets/mysql.png";
+import Java from "../assets/java.png";
+import Python from "../assets/python.png";
+import Tailwind from "../assets/tailwind.png";
+import MongoDB from "../assets/mongodb.png";
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-auto sm:h-screen bg-[#1B1C1E] text-gray-300'>
+    <div
+      name="skills"
+      className="w-full h-auto sm:h-screen bg-[#1B1C1E] text-gray-300"
+    >
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div>
-              <p className='text-4xl font-bold inline border-b-4 border-yellow-300 '>Skills</p>
-              <p className='py-4'>// List of my technical skills.</p>
-          </div>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-yellow-300 ">
+            Skills
+          </p>
+          <p className="py-4">// List of my technical skills.</p>
+        </div>
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                  <p className='my-4'>HTML</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
-                  <p className='my-4'>CSS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                  <p className='my-4'>JAVASCRIPT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                  <p className='my-4'>REACT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Flutter} alt="HTML icon" />
-                  <p className='my-4'>FLUTTER</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Dart} alt="HTML icon" />
-                  <p className='my-4'>DART</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mysql} alt="HTML icon" />
-                  <p className='my-4'>MYSQL</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
-                  <p className='my-4'>FIREBASE</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Java} alt="HTML icon" />
-                  <p className='my-4'>JAVA</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Python} alt="HTML icon" />
-                  <p className='my-4'>PYTHON</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
-                  <p className='my-4'>TAILWINDCSS</p>
-              </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4">HTML</p>
           </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+            <p className="my-4">Javascript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={TypeScript} alt="HTML icon" />
+            <p className="my-4">Typescript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+            <p className="my-4">React</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Flutter} alt="HTML icon" />
+            <p className="my-4">Flutter</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Dart} alt="HTML icon" />
+            <p className="my-4">Dart</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Mysql} alt="HTML icon" />
+            <p className="my-4">MySQL</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={MongoDB} alt="HTML icon" />
+            <p className="my-4">MongoDB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Java} alt="HTML icon" />
+            <p className="my-4">Java</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Python} alt="HTML icon" />
+            <p className="my-4">Python</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className="my-4">TailwindCSS</p>
+          </div>
+        </div>
       </div>
     </div>
   );
